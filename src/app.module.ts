3e28efb7e26436e4bc2eb9606c75d301b441278f
Sifat Hossain <sifathossain1326@gmail.com>
2026-03-09 02:09:51 +0600

    Add Skill Module with DTO, E

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,25 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { SkillsModule } from './modules/skills/skills.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: 5432,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      entities: [],
+      synchronize: true,
+    }),
+    SkillsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
