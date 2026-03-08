59cd6dadbb9db47a3ff76387b97a0316fb4a81e8
Sifat Hossain <sifathossain1326@gmail.com>
2026-03-09 01:33:00 +0600

    Setup Config Globally

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
+  constructor(private configServer: ConfigService) {}
   getHello(): string {
-    return 'Hello World!';
+    const dbName = this.configServer.get<string>('APP_NAME', 'DefaultValue');
+    return `Hello World! ${dbName}`;
   }
 }
